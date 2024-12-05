e55d338135a22cb339887769f13786c013c55b96
Franklin Kong <franklinkong981@gmail.com>
2024-12-05 12:17:41 -0800

    Added thorough documentation for the sqlForPartialUpdate function

---
+++ helpers/sql.test.js
@@ -0,0 +1,3 @@
+/* This is the file that tests the helper function sqlForPartialUpdate in helpers/sql.js, which is used to 
+create part of the SQL query string to partially update a user, company, or job. */
+
