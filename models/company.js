22e695a32b412a860938b39653aaefa65242a3c3
Franklin Kong <franklinkong981@gmail.com>
2024-12-10 22:26:00 -0800

    Changed Job model so that getting job with specific handle returns list of jobs too

--- models/company.js
+++ models/company.js
@@ -104,20 +104,34 @@ class Company {
 
   static async get(handle) {
     const companyRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           WHERE handle = $1`,
+          `SELECT c.handle, c.name, c.description, c.num_employees AS "numEmployees", c.logo_url AS "logoUrl",
+            j.id, j.title, j.salary, j.equity
+            FROM companies AS c
+            JOIN jobs AS j ON c.handle = j.company_handle
+            WHERE handle = $1`,
         [handle]);
 
     const company = companyRes.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
 
-    return company;
+    let companyJobs = companyRes.rows.map((j) => {
+      return {
+        id: j.id,
+        title: j.title,
+        salary: j.salary,
+        equity: j.equity
+      }
+    });
+
+    return {
+      handle: company.handle,
+      name: company.name,
+      description: company.description,
+      numEmployees: company.numEmployees,
+      logoUrl: company.logoUrl,
+      jobs: companyJobs
+    };
   }
 
   /** Update company data with `data`.
